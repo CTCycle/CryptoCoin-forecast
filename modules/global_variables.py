d2429eb9b83dab11954c8aa003534f89eba41963
CTCycle <thomas.virdis89@gmail.com>
2023-10-21 12:21:07 +0200

    changed global variables

--- modules/global_variables.py
+++ modules/global_variables.py
@@ -1,13 +1,15 @@
 import pandas as pd
 
-# variables for data series recognition
-#==============================================================================
+# data storage variables
+#------------------------------------------------------------------------------
 list_of_files = []
 dataframe_name = ''
 dataframe = pd.DataFrame()
 preprocessed_data = {}
 normalizer = ''
 
+# plot object placeholders
+#------------------------------------------------------------------------------
 ML_canvas_status = False
 history_plot = None
 
